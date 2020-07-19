6f2c2bd8fbe2572d3f6530801d35539fcf6d7128
cflw <cflw@outlook.com>
2020-07-19 08:34:26 +0800

    20200719

--- 知识库/admin.py
+++ 知识库/admin.py
@@ -12,6 +12,19 @@ class C文章管理(admin.ModelAdmin):
 		"m上级文档",
 	]
 	list_display = ("m名称", "m标题", "mi目录显示", "m排列位置")
+	list_editable = ("mi目录显示", "m排列位置")
+	#动作
+	actions = ["f排列位置加一百", "f排列位置减一百"]
+	def f排列位置加一百(self, a请求, a查询集):
+		for v记录 in a查询集:
+			v记录.m排列位置 += 100
+			v记录.save()
+	f排列位置加一百.short_description = "排列位置 +100"
+	def f排列位置减一百(self, a请求, a查询集):
+		for v记录 in a查询集:
+			v记录.m排列位置 -= 100
+			v记录.save()
+	f排列位置减一百.short_description = "排列位置 -100"
 
 admin.site.register(models.C文档表, C文章管理)
 admin.site.register(models.C文件表)
